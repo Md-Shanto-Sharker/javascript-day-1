bfed844402efd41f4c225c716adbd2ca8d6e2a22
SHANTO <rabbymd55555@gmail.com>
2025-02-07 22:09:20 +0600

    added isNaN problem file

---
+++ mixedOp.js
@@ -0,0 +1,10 @@
+// use  the concat nation operator
+
+var first = 'md shanto';
+var second = ' sharker';
+console.log(first + second);
+
+const a = isNaN('11');
+const b = isNaN(2-10);
+console.log(a); 
+console.log(b); 
---
+++ short-hand.js
@@ -0,0 +1,23 @@
+// var x = 10;
+// x=x+30;
+// use the short hand operator
+// x+=30
+// console.log(x);
+
+
+// var y = 20;
+// y-=3;
+// console.log(y);
+
+
+
+// var fridge  = 35000;
+// fridge = fridge - 1000;
+// use the short hand operator
+// fridge -= 1000;
+// console.log(fridge);
+
+
+var apple = 200;
+apple*=5;
+console.log(apple);
